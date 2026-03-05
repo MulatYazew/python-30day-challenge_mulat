453daaae89deac936d643f5c94fd38f5b47328e4
MulatYazew <mulatyazewchekol>
2026-03-05 14:16:42 +0100

    some edition on day 07

--- exercises/day_07_sets/sets.py
+++ exercises/day_07_sets/sets.py
@@ -53,3 +53,6 @@ print(len(age) == len(age_set))
     Example: {1, 2, 3, 'Hello', True}
 """
 #I am a teacher and I love to inspire and teach people. How many unique words have been used in the sentence? Use the split methods and set to get the unique words.
+sentence = "I am a teacher and I love to inspire and teach people."
+unique_words = set(sentence.split())
+print(len(unique_words))
